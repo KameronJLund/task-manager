1c59d891a7bea89685366b7d46616bbfc0e53a03
Kameron Lund <klund-airtank@Kameron-Lund.local>
2022-07-01 12:50:30 -0600

    minor housekeeping following lesson

--- mongodb.js
+++ mongodb.js
@@ -1,53 +1,14 @@
-const mongodb = require('mongodb')
-const MongoClient = mongodb.MongoClient
+const { MongoClient, ObjectID } = require('mongodb')
 
 const connectionUrl = 'mongodb://127.0.0.1:27017'
 const databaseName = 'task-manager'
 
-MongoClient.connect(connectionUrl, { useNewUrlParser: true, useUnifiedTopology: true }, (error, client) => {
-    if (error) {
-        return console.log('unable to connect to mongoDB')
-    }
+MongoClient.connect(connectionUrl, { useNewUrlParser: true, useUnifiedTopology: true },
+    (error, client) => {
+        if (error) {
+            return console.log('unable to connect to mongoDB')
+        }
 
-    const db = client.db(databaseName)
+        const db = client.db(databaseName)
 
-    // db.collection('users').insertOne({
-    //     name: 'Kameron',
-    //     age: 19
-    // }, (error, result) => {
-    //     if (error) return console.log('unable to insert user')
-
-    //     console.log(result.ops)
-    // })
-
-    // db.collection('users').insertMany([
-    //     {
-    //         name: 'kameron',
-    //         age: 19
-    //     }, {
-    //         name: 'andrew',
-    //         age: 27
-    //     },
-    // ], (error, result) => {
-    //     if (error) console.log('unable to insert users')
-
-    //     console.log(result.ops)
-    // })
-
-    db.collection('tasks').insertMany([
-        {
-            description: 'mop',
-            completed: false
-        }, {
-            description: 'sweep',
-            completed: true
-        }, {
-            description: 'dust',
-            completed: false
-        },
-    ], (error, result) => {
-        if (error) console.log('unable to insert collections')
-
-        console.log(result.ops)
     })
-})
